a6f98e964e63bd0648959b61cd252861242fb779
Zhiduo Zhang <zhiduo.zhang.aus@gmail.com>
2021-08-31 13:52:49 +1000

    Fix Memory Issue with TIF

--- recorder.m
+++ recorder.m
@@ -1,34 +1,41 @@
 classdef recorder < handle
     properties
         diskLogger % disk object
+        first = true
+        filepath
     end
     methods
         function obj = recorder()
         end
         
         function save_frame(obj, camera, ~)
-            frame = im2uint8(peekdata(camera,1));
-            size(frame);
-            writeVideo(obj.diskLogger, frame);
+            if obj.first
+                obj.first = false;
+                imwrite(im2uint8(getsnapshot(camera)),obj.filepath);
+            else
+                imwrite(im2uint8(getsnapshot(camera)),obj.filepath,'WriteMode','append');
+            end
+            flushdata(camera);
+            %writeVideo(obj.diskLogger, im2uint8(getsnapshot(camera)));
         end
         
         function start(obj, camera, filepath, frame_grab_interval)
-            obj.diskLogger = VideoWriter(filepath, 'Grayscale AVI');
-            if strcmp(camera.model, 'blackfly_s')
-                obj.diskLogger.FrameRate = camera.src.AcquisitionFrameRate;
-            end
+            %obj.diskLogger = VideoWriter(filepath, 'MPEG-4');
+            %if strcmp(camera.model, 'blackfly_s')
+            %    obj.diskLogger.FrameRate = camera.src.AcquisitionFrameRate/frame_grab_interval;
+            %end
             %camera.vid.DiskLogger = obj.diskLogger;
             
-            
+            obj.filepath = filepath;
             camera.vid.FramesAcquiredFcnCount = frame_grab_interval;
             camera.vid.FramesAcquiredFcn = @obj.save_frame;
             
             start(camera.vid);
-            open(obj.diskLogger);
+            %open(obj.diskLogger);
         end
         function stop(obj, camera)
             stop(camera.vid);
-            close(obj.diskLogger);
+            %close(obj.diskLogger);
         end
         function [frame] = snap(~, camera)
             frame = getsnapshot(camera.vid);
