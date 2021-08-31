9da7a3c7a33181b36d5cc00fdce80de00dea2573
Zhiduo Zhang <zhiduo.zhang.aus@gmail.com>
2021-08-31 13:59:07 +1000

    Fix Memory issues AVI

--- recorder.m
+++ recorder.m
@@ -9,33 +9,27 @@ classdef recorder < handle
         end
         
         function save_frame(obj, camera, ~)
-            if obj.first
-                obj.first = false;
-                imwrite(im2uint8(getsnapshot(camera)),obj.filepath);
-            else
-                imwrite(im2uint8(getsnapshot(camera)),obj.filepath,'WriteMode','append');
-            end
+            writeVideo(obj.diskLogger, im2uint8(getsnapshot(camera)));
             flushdata(camera);
-            %writeVideo(obj.diskLogger, im2uint8(getsnapshot(camera)));
         end
         
         function start(obj, camera, filepath, frame_grab_interval)
-            %obj.diskLogger = VideoWriter(filepath, 'MPEG-4');
-            %if strcmp(camera.model, 'blackfly_s')
-            %    obj.diskLogger.FrameRate = camera.src.AcquisitionFrameRate/frame_grab_interval;
-            %end
-            %camera.vid.DiskLogger = obj.diskLogger;
+            obj.diskLogger = VideoWriter(filepath, 'Grayscale AVI');
+            if strcmp(camera.model, 'blackfly_s')
+                obj.diskLogger.FrameRate = camera.src.AcquisitionFrameRate/frame_grab_interval;
+            end
+            camera.vid.DiskLogger = obj.diskLogger;
             
             obj.filepath = filepath;
             camera.vid.FramesAcquiredFcnCount = frame_grab_interval;
             camera.vid.FramesAcquiredFcn = @obj.save_frame;
             
             start(camera.vid);
-            %open(obj.diskLogger);
+            open(obj.diskLogger);
         end
         function stop(obj, camera)
             stop(camera.vid);
-            %close(obj.diskLogger);
+            close(obj.diskLogger);
         end
         function [frame] = snap(~, camera)
             frame = getsnapshot(camera.vid);
